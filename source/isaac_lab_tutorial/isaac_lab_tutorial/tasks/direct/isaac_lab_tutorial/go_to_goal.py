1d817f8aa935d657e44ad9d246b3c0409969e27b
shotaro-sharkle1112 <112090211+shotaro-sharkle1112@users.noreply.github.com>
2025-10-03 17:14:57 +0900

    Update go_to_goal.py

--- source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/go_to_goal.py
+++ source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/go_to_goal.py
@@ -180,9 +180,9 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
         elif learning_state == 3: # act
             left_torque = pm[0][0]
             right_torque = pm[0][1]
-
-            print(f"[INFO]: obs {obs}")
-            print(f"[INFO]: left {left_torque}, right {right_torque}")
+            if steps % 20 == 0:
+                print(f"[INFO]: obs {obs}")
+                print(f"[INFO]: left {left_torque}, right {right_torque}")
             
             torque = -10.0 * torch.tensor([[left_torque, right_torque,left_torque,right_torque]])
             # limoのトルクをかける
