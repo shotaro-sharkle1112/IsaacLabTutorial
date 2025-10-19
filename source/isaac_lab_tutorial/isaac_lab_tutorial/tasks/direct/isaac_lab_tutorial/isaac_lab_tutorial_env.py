3206c7ec27620514d631664a777914c994d473c9
shotaro-sharkle1112 <112090211+shotaro-sharkle1112@users.noreply.github.com>
2025-10-20 00:32:58 +0900

    Update isaac_lab_tutorial_env.py

--- source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/isaac_lab_tutorial_env.py
+++ source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/isaac_lab_tutorial_env.py
@@ -341,7 +341,6 @@ class LimoPendulumNoNoiseEnv(DirectRLEnv):
         # 各環境の重さのランダマイズ
         for i in env_ids:
             m = sample_uniform(self.cfg.weight_range[0],self.cfg.weight_range[1],1,joint_pos.device).item()
-            print("[DEBUG]:mass radomization",i,"mass", m)
             sim_utils.schemas.modify_mass_properties(
                 prim_path=f"/World/envs/env_{i}/Robot/weight_link",
                 cfg=sim_utils.schemas.MassPropertiesCfg(mass=m)
