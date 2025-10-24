b7c091aa4aa03b1d929d5dba703bb614ff84ed3e
shotaro-sharkle1112 <112090211+shotaro-sharkle1112@users.noreply.github.com>
2025-10-24 23:08:34 +0900

    Update run_script.bat

--- run_script.bat
+++ run_script.bat
@@ -6,16 +6,16 @@ cd /d "%~dp0"
 
 
 
-python scripts\rsl_rl\train.py --task Limo-Pendulum-NoNoise --max_iterations 3 --load_run 2025-10-24_22-32-19 --checkpoint model_4750.pt --resume --headless --video
+python scripts\rsl_rl\train.py --task Limo-Pendulum-NoNoise --max_iterations 2000 --load_run 2025-10-24_22-32-19 --checkpoint model_4750.pt --resume --headless --video
 
 echo [1/3] Running Limo-Pendulum-NoNoise-R2-64...
-python scripts\rsl_rl\train.py --task Limo-Pendulum-NoNoise-R2-64 --max_iterations 3 --headless --video
+python scripts\rsl_rl\train.py --task Limo-Pendulum-NoNoise-R2-64 --max_iterations 5000 --headless --video
 
 echo [2/3] Running Limo-Pendulum-NoNoise-R2-32...
-python scripts\rsl_rl\train.py --task Limo-Pendulum-NoNoise-R2-32 --max_iterations 3 --headless --video
+python scripts\rsl_rl\train.py --task Limo-Pendulum-NoNoise-R2-32 --max_iterations 5000 --headless --video
 
 echo [3/3] Running Limo-Pendulum-NoNoise-R2-128...
-python scripts\rsl_rl\train.py --task Limo-Pendulum-NoNoise-R2-128 --max_iterations 3 --headless --video
+python scripts\rsl_rl\train.py --task Limo-Pendulum-NoNoise-R2-128 --max_iterations 5000 --headless --video
 
 echo.
 echo Done.
