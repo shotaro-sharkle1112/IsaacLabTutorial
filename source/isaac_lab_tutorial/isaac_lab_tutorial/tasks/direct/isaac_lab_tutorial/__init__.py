ad474f759d69225367f1574f5c16e4f5498638bf
shotaro-sharkle1112 <112090211+shotaro-sharkle1112@users.noreply.github.com>
2025-10-26 17:38:16 +0900

    Add new Limo-Pendulum-NoNoise-R2-32-16 environment and PPO config
    Registered a new Gym environment 'Limo-Pendulum-NoNoise-R2-32-16' with specific configuration entries. Added a new PPO runner configuration class with 32-16 hidden dimensions to support the new environment.

--- source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/__init__.py
+++ source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/__init__.py
@@ -143,3 +143,15 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg128",
     },
 )
+
+gym.register(
+    id="Limo-Pendulum-NoNoise-R2-32-16",
+    entry_point=f"{__name__}.isaac_lab_tutorial_env:LimoPendulumNoNoiseEnv2",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.isaac_lab_tutorial_env_cfg:LimoPendulumEnvCfg",
+        "skrl_amp_cfg_entry_point": f"{agents.__name__}:skrl_amp_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_limo_pendulum_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg32-16",
+    },
+)
