9a43efe11e723cdc8651979017575c7310acbd75
shotaro-sharkle1112 <112090211+shotaro-sharkle1112@users.noreply.github.com>
2025-11-22 13:43:17 +0900

    Increase action_scale to 60.0 in LimoPendulumEnvCfg
    The action_scale parameter in LimoPendulumEnvCfg was increased from 30.0 to 60.0, likely to allow for a wider range of actions in the environment.

--- source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/isaac_lab_tutorial_env_cfg.py
+++ source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/isaac_lab_tutorial_env_cfg.py
@@ -55,7 +55,7 @@ class LimoPendulumEnvCfg(DirectRLEnvCfg):
     # env
     decimation = 2
     episode_length_s = 30.0
-    action_scale = 30.0
+    action_scale = 60.0
     # - spaces definition
     action_space = 1
     # observation_space = 9
