71c3c13baa8c4abc9095812d702b303b08302791
shotaro-sharkle1112 <112090211+shotaro-sharkle1112@users.noreply.github.com>
2025-11-28 22:47:11 +0900

    Update isaac_lab_tutorial_env_cfg.py

--- source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/isaac_lab_tutorial_env_cfg.py
+++ source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/isaac_lab_tutorial_env_cfg.py
@@ -75,7 +75,7 @@ class LimoPendulumEnvCfg(DirectRLEnvCfg):
     weight_range = [0.1, 0.3]
     # reward scales
     rew_scale_alive = 1.0
-    rew_scale_terminated = -2.0
+    rew_scale_terminated = 0.0
     rew_scale_pole_pos = -1.0
     rew_scale_pole_vel = -0.01
     rew_scale_cart_vel = -0.0001
