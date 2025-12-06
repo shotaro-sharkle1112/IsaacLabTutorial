bf19ced4c3d26b3d3b45891de12934b1f2021c69
shotaro-sharkle1112 <112090211+shotaro-sharkle1112@users.noreply.github.com>
2025-12-06 19:05:23 +0900

    Update isaac_lab_tutorial_env_cfg.py

--- source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/isaac_lab_tutorial_env_cfg.py
+++ source/isaac_lab_tutorial/isaac_lab_tutorial/tasks/direct/isaac_lab_tutorial/isaac_lab_tutorial_env_cfg.py
@@ -55,7 +55,7 @@ class LimoPendulumEnvCfg(DirectRLEnvCfg):
     # env
     decimation = 2
     episode_length_s = 30.0
-    action_scale = 50.0
+    action_scale = 40.0
     # - spaces definition
     action_space = 1
     # observation_space = 9
@@ -71,7 +71,7 @@ class LimoPendulumEnvCfg(DirectRLEnvCfg):
     pole_dof_name = ["pendulum"]
 
     max_cart_pos = 10000.0  # the cart is reset if it exceeds that position [m]
-    initial_pole_angle_range = [-0.05, 0.05]  # the range in which the pole angle is sampled from on reset [rad]
+    initial_pole_angle_range = [-0.1, 0.1]  # the range in which the pole angle is sampled from on reset [rad]
     weight_range = [0.5, 0.5]
     # reward scales
     rew_scale_alive = 1.0
